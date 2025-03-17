92434026da7d69210780e7c3933af2f52d038e9e
Kiko <89874363+KikoMedina@users.noreply.github.com>
2025-03-17 15:30:28 +0100

    display error message

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const NEWS_API = "https://newsapi.org/v2/everything";
 const API_KEY = "";
 
 async function fecthNews() {
-    
+    const language = document.getElementById("sprache").value;
+    const url = `${NEWS_API}?q=gehirn&sprache=${sprache}&sortBy=publishedAt&apiKey=${API_KEY}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        displayNews(data.articles);
+    } catch (error) {
+        console.error("Fehler beim Abrufen der Nachrichten", error);
+    }
 }
