6bf24ba04fea5b7a3b5aab73f4a046216ce54201
Kiko <89874363+KikoMedina@users.noreply.github.com>
2025-04-03 13:24:40 +0200

    Finish Function displayNews

--- script.js
+++ script.js
@@ -16,5 +16,24 @@ async function fecthNews() {
 }
 
 function displayNews(articles)  {
-    
+    const newsContainer = document.getElementById("news");
+    newsContainer.innerHTML = "";
+
+    if (articles.length === 0) {
+        newsContainer.innerHTML = "<p>Keine Nachrichten gefunden</p>";
+        return;
+    }
+
+    articles.forEach(article => {
+        const newsCard = document.createElement("div");
+        newsCard.classList.add("news-card");
+
+        newsCard.innerHTML =
+            <h3>${article.title}</h3>
+            <p>${article.description || "Keine Beschreibung verfügbar."}</p>
+            <a href="${article.url}" target="_blank">Mehr lesen</a>
+        ;
+
+        newsContainer.appendChild(newsCard);
+    });
 }
